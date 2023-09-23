563a0de290c9708a40186fb763f789e5c3aebb8a
BananaWong <bananawongh@gmail.com>
2023-09-23 13:50:42 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const GEOCODING_API_KEY = 'AIzaSyCkRbyonCvO0212wyJYH64jpQKu2jhKVzU';
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbwovoVTkLBa2kJb28_h1JGDwncbQVGyxUu5d2ld0pTHRHiGj2i7Iy9fl_OAfVTZpzWAtw/exec'; 
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwV5kC4C0fJ4kwrPRUL9RBMrgLILX0o0AP4Ve5eKnu7ofvGZFuEcLbwoxX19Ya02srAvA/exec'; 
 
 document.getElementById('signInBtn').addEventListener('click', function() {
     handleAttendance('signIn');
@@ -12,7 +12,8 @@ document.getElementById('signOutBtn').addEventListener('click', function() {
 function handleAttendance(action) {
     const employeeName = document.getElementById('employeeName').value.trim();
     const note = document.getElementById('note').value.trim();
-
+    const expectedStartTime = document.getElementById('startTime').value;
+    const expectedEndTime = document.getElementById('endTime').value;
     if (!employeeName) {
         alert('請輸入姓名 / Please enter your name');
         return;
@@ -30,7 +31,9 @@ function handleAttendance(action) {
                 location: placeName,
                 note: note,
                 lat: lat,
-                lng: lng
+                lng: lng,
+                expectedStartTime: expectedStartTime,
+                expectedEndTime: expectedEndTime,
             };
             addRecordToSheet(data);
         });
@@ -69,7 +72,7 @@ function addRecordToSheet(data) {
     .then(response => {
         if (response.type === 'opaque') {
             // 无法访问响应内容，但请求可能已成功
-            alert('記錄可能已成功，請檢查後端數據。 / The record may have been successful, please check the backend data.');
+            alert('記錄請求已發送，請以實際資料爲準。 / The record may have been successful, please check the backend data.');
         } else {
             // 处理其他情况，例如解析 JSON 响应
             return response.json().then(json => {
@@ -82,6 +85,6 @@ function addRecordToSheet(data) {
         }
     })
     .catch(error => {
-        alert('網絡錯誤，請稍後重試。 / Network error, please try again later.');
+        alert('網路錯誤，請稍後重試。 / Network error, please try again later.');
     });
 }
